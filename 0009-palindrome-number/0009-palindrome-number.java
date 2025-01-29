7c4427ece028dcbd794062beedf3770b9d518eec
KANMANIPRIYA S <kanmanipriyas.22cse@kongu.edu>
2025-01-29 11:24:16 +0530

    Time: 186 ms (5.96%), Space: 44.8 MB (5.5%) - LeetHub

--- 0009-palindrome-number/0009-palindrome-number.java
+++ 0009-palindrome-number/0009-palindrome-number.java
@@ -1,17 +1,20 @@
 class Solution {
     public boolean isPalindrome(int x) {
+        int sum=0;
+        int val=x;
         if(x>=0)
         {
-        List<Integer> l1=new ArrayList<>();
-        while(x!=0)
-        {
-            int r=x%10;
-            x=x/10;
-            l1.add(r);
-        }
-        List<Integer> l2=new ArrayList<>(l1);
-        Collections.reverse(l1);
-        return l1.equals(l2);
+            while(x!=0)
+            {
+                int r=x%10;
+                x=x/10;
+                sum=sum*10+r;
+                System.out.println(sum);
+            }
+            if(sum==val)
+            {
+                return true;
+            }
         }
         return false;
     }
