0f4a6e7874a97b736c02ececa4c1ffd98abab1f1
KANMANIPRIYA S <kanmanipriyas.22cse@kongu.edu>
2025-01-30 11:16:03 +0530

    Time: 15 ms (20.05%), Space: 57.2 MB (57.72%) - LeetHub

--- 0041-first-missing-positive/0041-first-missing-positive.java
+++ 0041-first-missing-positive/0041-first-missing-positive.java
@@ -4,11 +4,11 @@ class Solution {
         int val=1;
         for(int i=0;i<nums.length;i++)
         {
-            if(nums[i]>0 && nums[i]==val)
+            if(nums[i]==val)
             {
                 val++;
             }
-            if(nums[i]>0 && nums[i]>val)
+            if(nums[i]>val)
             {
                 return val;
             } 
