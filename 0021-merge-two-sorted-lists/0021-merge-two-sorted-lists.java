65453589a31a74aac82128fec917cd31ea01d7ca
KANMANIPRIYA S <kanmanipriyas.22cse@kongu.edu>
2025-02-13 15:39:45 +0530

    Time: 0 ms (100%), Space: 42.5 MB (58.5%) - LeetHub

--- 0021-merge-two-sorted-lists/0021-merge-two-sorted-lists.java
+++ 0021-merge-two-sorted-lists/0021-merge-two-sorted-lists.java
@@ -17,15 +17,14 @@ class Solution {
             if(list1.val<list2.val)
             {
                 current.next=list1;
-                current=current.next;
                 list1=list1.next;
             }
             else
             {
                 current.next=list2;
-                current=current.next;
                 list2=list2.next;
             }
+            current=current.next;
         }
         current.next=(list1!=null)?list1:list2;
         return dummy.next;
