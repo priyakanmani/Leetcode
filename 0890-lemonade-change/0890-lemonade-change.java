0eaf7a3d8b726dca447121cdf77ce1932111341f
KANMANIPRIYA S <kanmanipriyas.22cse@kongu.edu>
2025-04-30 14:53:41 +0530

    Time: 49 ms (5.48%), Space: 58.6 MB (5.03%) - LeetHub

--- 0890-lemonade-change/0890-lemonade-change.java
+++ 0890-lemonade-change/0890-lemonade-change.java
@@ -6,54 +6,29 @@ class Solution {
         {
            h1.put(bills[i],h1.getOrDefault(bills[i], 0) + 1);
 
-            if(bills[i]!=5)
+            if(bills[i]>5)
             {
                 int n=bills[i];
                   n=n-5;
-                
-                    if(n==15)
-                    {
-                       if ((h1.get(10) == null || h1.get(10) == 0) || (h1.get(5) == null || h1.get(5) == 0))
-
-                        {
-                            if((h1.get(10) == null || h1.get(10) == 0) && h1.get(5)!=null && (h1.get(5)>=3))
-                            {
-                               res=true;
-                               
-                               int v2=h1.get(5);
-                            
-                                h1.replace(5,v2-3);
-                        
-                            }
-                            else
-                            {
-                                res=false;
-                                break;
-                            }
-                            
-                        }
-                        else
-                        {
-                            int v1=h1.get(10);
-                            int v2=h1.get(5);
-                            h1.replace(10,v1-1);
-                            h1.replace(5,v2-1);
-                        }
-                    }
-                    else if(n==5)
-                    {
-                       
-                        if(h1.get(5)==null||h1.get(5)==0)
-                        {
-                            res=false;
-                            break;
-                        }
-                        else
-                        {
-                            int v=h1.get(5);
-                            h1.replace(5,v-1);
-                        }
-                    }
+                  while(n!=0)
+                  {
+                     if(n>10 && h1.get(10)!=null && h1.get(10)!=0)
+                     {
+                            h1.replace(10,h1.get(10)-1);
+                            n=n-10;
+                     }
+                     if(n>=5 && h1.get(5)!=null && h1.get(5)!=0)
+                     {
+                        h1.replace(5,h1.get(5)-1);
+                        n=n-5;
+                     }
+                     else
+                     {
+                        res=false;
+                        break;
+                     }
+                  }
+                    
                     
                  
             }
