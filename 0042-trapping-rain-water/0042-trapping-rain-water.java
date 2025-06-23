bea4b9629cd20c8dabf5279d89e022e0092f21b6
KANMANIPRIYA S <kanmanipriyas.22cse@kongu.edu>
2025-06-23 11:09:49 +0530

    Time: 1 ms (63%), Space: 46.3 MB (67.06%) - LeetHub

--- 0042-trapping-rain-water/0042-trapping-rain-water.java
+++ 0042-trapping-rain-water/0042-trapping-rain-water.java
@@ -1,24 +1,23 @@
 class Solution {
     public int trap(int[] height) {
-        int n=height.length;
-        int left[]=new int[n];
-        int right[]=new int[n];
-        int maxamount=0;
+        int left[]=new int[height.length];
         left[0]=height[0];
-        for(int i=1;i<n;i++)
+        for(int i=1;i<height.length;i++)
         {
-           left[i]=Math.max(left[i-1],height[i]);
+            left[i]=Math.max(left[i-1],height[i]);
         }
-        right[n-1]=height[n-1];
-        for(int i=n-2;i>=0;i--)
+        int right[]=new int[height.length];
+        right[height.length-1]=height[height.length-1];
+        for(int i=height.length-2;i>=0;i--)
         {
-            right[i]=Math.max(right[i+1],height[i]);
+            right[i]=Math.max(height[i],right[i+1]);
         }
-        for(int i=0;i<n;i++)
+        int result=0;
+        for(int i=0;i<height.length;i++)
         {
-            int s=Math.min(left[i],right[i]);
-            maxamount=maxamount+(s-height[i]);
+            int totalsum=Math.min(left[i],right[i]);
+            result=result+(totalsum-height[i]);
         }
-        return maxamount;
+        return result;
     }
 }
