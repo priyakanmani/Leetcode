3eda2a4ad87e52ce305a916ddf8bad92751a8fde
KANMANIPRIYA S <kanmanipriyas.22cse@kongu.edu>
2025-06-23 18:51:38 +0530

    Time: 2 ms (94.23%), Space: 44.9 MB (62.82%) - LeetHub

--- 3540-hash-divided-string/3540-hash-divided-string.java
+++ 3540-hash-divided-string/3540-hash-divided-string.java
@@ -1,23 +1,23 @@
 class Solution {
     public String stringHash(String s, int k) {
-        HashMap<Character,Integer> h1=new HashMap<>();
+       
        
         StringBuilder str=new StringBuilder();
-        int sum1=0;
+        int sum=0;
         int i=0;
 
         while(i<s.length())
         {
-            
+            sum=0;
             for(int j=0;j<k;j++,i++)
             {
-              h1.put(s.charAt(i),s.charAt(i)-97);
-              sum1=sum1+(s.charAt(i)-97);
+              
+              sum=sum+(s.charAt(i)-97);
               
             }
-            str.append((char)((sum1%26)+97));
-            sum1=0;
-            h1.clear();
+            str.append((char)((sum%26)+97));
+            
+           
         }
         
        
